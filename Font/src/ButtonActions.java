7d6d55c6a8388259804517556864276a47d122d8
Justin <Justin@justingmacbook2>
2014-09-06 16:29:27 -0700

    Added search field

---
+++ Font/src/ButtonActions.java
@@ -0,0 +1,53 @@
+
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+
+public class ButtonActions {
+
+	public static void OpenInFinder( String PI)
+	{
+    	try
+    	{
+        ScriptEngineManager mgr = new ScriptEngineManager();        
+        ScriptEngine engine = mgr.getEngineByName("AppleScript");
+        engine.eval("property the_path : \"" + PI + "\" \n set the_folder to (POSIX file the_path) as alias \n tell application \"Finder\" \n activate \n reveal the_folder \n end tell");
+//        engine.eval( "tell application \"Finder\" \n set commento to \""+Comment+"\" \n set TheFile to \""+MACpath+FileName+"\" \n set comment of (TheFile as alias) to commento \n end tell" );
+        
+    	}
+    	catch(Exception e)
+    	{
+    		System.out.println("Error");
+    	}
+
+	}
+	
+//	public static final ActionListener OIF = new ActionListener() 
+//	{
+//		public void actionPerformed(ActionEvent arg0)
+//		{
+//			OpenInFinder();
+//		}
+//	};
+
+}
