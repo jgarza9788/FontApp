c91fe641641697b6512269fda355cb6195ed9864
Justin <Justin@justingmacbook2>
2014-09-06 16:27:27 -0700

    Added Search Field, and separators

--- Font/src/Window.java
+++ Font/src/Window.java
@@ -25,7 +25,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneLayout;
+import javax.swing.SwingConstants;
+
 import java.awt.FontMetrics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -35,9 +38,9 @@ import java.awt.image.BufferedImage;
 public class Window
 {
 
-//	private static final int String = 0;
-
 	public static JFrame ThisWindow = new JFrame("FontApp");
+	
+	public JPanel SearchArea = new JPanel();
 	
 	public JPanel Content = new JPanel();
 	public JScrollPane ScrollPane = new JScrollPane(Content);
@@ -50,8 +53,11 @@ public class Window
 	public ArrayList<String> AllFontNames = new ArrayList<String>();
 	public ArrayList<String> AllFontPaths = new ArrayList<String>();
 	
+	public ArrayList<JLabel> AllFontLabels = new ArrayList<JLabel>();
+	
 	public Window() 
 	{
+		AllFontLabels.clear();
 		
 		JFrame PleaseWait = new JFrame();
 //		PleaseWait.getContentPane.setlayout(new BorderLayout());
@@ -60,12 +66,16 @@ public class Window
         ThisWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ThisWindow.setVisible(false);
         
+        
+        
         Content.setLayout(new GridBagLayout());
 //        Content.setLayout(new BorderLayout(5,5));
         Content.setBackground(Color.WHITE);
         ScrollPane.getVerticalScrollBar().setUnitIncrement(10);
         
-        ThisWindow.getContentPane().add(ScrollPane);
+        ThisWindow.getContentPane().add(SearchArea, BorderLayout.PAGE_START);
+        
+        ThisWindow.getContentPane().add(ScrollPane, BorderLayout.CENTER);
         ScrollPane.setLayout(new ScrollPaneLayout());
         
         ThisWindow.setMinimumSize(new Dimension(350, 350));
@@ -80,6 +90,8 @@ public class Window
 		PleaseWait.setBounds( PosXofWindow, PosYofWindow,300, 150);
 		PleaseWait.setVisible(true);
 		
+		
+		AddSearchBar();
 		CollectContent();
 		AddContent();
 		
@@ -89,6 +101,50 @@ public class Window
 		
 		PleaseWait.removeAll();
 		
+	}
+	
+	public void AddSearchBar()
+	{
+		JLabel ThisLabel = new JLabel("Search");
+		SearchArea.add(ThisLabel, BorderLayout.LINE_START);
+		
+		final JTextField SearchBox =  new JTextField(20);
+//		SearchBox.setSize(100,125);
+		SearchArea.add(SearchBox, BorderLayout.LINE_END);
+		
+		SearchBox.addActionListener(new ActionListener() 
+		{
+		      public void actionPerformed(ActionEvent e) 
+		      {
+		    	  Boolean FoundOne = false;
+
+		    	  for (int i = 0; i < AllFontLabels.size() ; i ++)
+		    	  {
+		    		  System.out.println(AllFontLabels.get(i).getText().toUpperCase().indexOf(SearchBox.getText().toUpperCase()));
+		    				  
+		    		  if (AllFontLabels.get(i).getText().toUpperCase().indexOf(SearchBox.getText().toUpperCase()) >= 0 )
+		    		  {
+		    			  if (FoundOne == false)
+		    			  {
+			    			  Content.scrollRectToVisible(AllFontLabels.get(AllFontLabels.size() -1).getBounds());
+			    			  Content.scrollRectToVisible(AllFontLabels.get(i).getBounds());
+			    			  FoundOne = true;
+		    			  }
+
+		    			  AllFontLabels.get(i).setOpaque(true);
+		    			  AllFontLabels.get(i).setBackground(Color.yellow);
+
+		    		  }
+		    		  else
+		    		  {
+		    			  AllFontLabels.get(i).setOpaque(false);
+		    			  AllFontLabels.get(i).setBackground(Color.white);
+		    		  }
+		    			  
+		    		  
+		    	  }
+		      }
+		});
 	}
 	
 	public void CollectContent()
@@ -123,17 +179,6 @@ public class Window
 		
 		Collections.sort(AllFontNames.subList(1, AllFontNames.size()));
 		Collections.sort(AllFontPaths.subList(1, AllFontPaths.size()));
-		
-		
-		
-//		for (String F : AllFontNames)
-//		{
-//			System.out.println(F);
-//		}
-//		for (String P : AllFontPaths)
-//		{
-//			System.out.println(P);
-//		}
 		
 	}
 
@@ -172,13 +217,8 @@ public class Window
 				{
 					BufferedImage bi = new BufferedImage(5, 5, BufferedImage.TYPE_INT_RGB);
 					FontMetrics fm = bi.getGraphics().getFontMetrics(new Font(font.getName(), Font.PLAIN, FontSize));
-//					FontPixelHeight = fm.getHeight();
 					FontPixelHeight = fm.getAscent();
-//					FontPixelHeight = fm.getAscent();
-					
-					
-//					System.out.println(FontPixelHeight);
-					
+
 					if (FontPixelHeight >= 20 )
 					{
 						LoopAgain = false;
@@ -187,7 +227,6 @@ public class Window
 					else 
 					{
 						FontSize = FontSize + 1;
-//						System.out.println(FontPixelHeight);
 					}
 					System.out.println(FontSize);
 					
@@ -201,6 +240,8 @@ public class Window
 				SampleLabel.setFont(new Font(font.getName(), Font.PLAIN, FontSize));
 				JLabel SpacerLabel = new JLabel(" ");
 				SpacerLabel.setFont(new Font(font.getName(), Font.PLAIN, FontSize));
+				
+				AllFontLabels.add(FontLabel);
 					
 				GridBagConstraints GBC = new GridBagConstraints();
 				GBC.fill = GridBagConstraints.HORIZONTAL;
@@ -212,7 +253,6 @@ public class Window
 	        	GBC.insets = new Insets(5,10,0,10);
 	        	
 	        	Content.add(FontLabel,GBC);
-	        	
 	        	
 	        	y = y + 1;
 	        	GBC.gridx = 0;
@@ -249,32 +289,17 @@ public class Window
 	        	GBC.gridy = y;
 	        	GBC.insets = new Insets(0,0,0,0);
 	        	
-	        	Content.add(new JSeparator(),GBC);
+	    		JLabel Separator = new JLabel("---------------------------------------------");
+	    		Separator.setForeground(Color.lightGray);
+	    		Content.add(Separator , GBC);
+	        	
+//	        	JSeparator JS = new JSeparator();
+//	        	Content.add(JS,GBC);
+//	        	Content.add(new JSeparator(),GBC);
+//	        	Content.add(new JSeparator(SwingConstants.HORIZONTAL),GBC);
 	        	
 	        	y = y + 1;
 	        	GBC.gridy = y;
-	        	
-	        	
-				
-//				FontLabel.setSize(500,500);
-//				SampleLabel.setSize(500,500);
-//				SpacerLabel.setSize(500,500);
-//				
-//				FontLabel.setLocation(50, y);
-//				
-//				y = y + 250;
-//				
-//				SampleLabel.setLocation(50, y);
-//				
-//				y = y + 250;
-//				
-//				SpacerLabel.setLocation(50, y);
-//				
-//				y = y + 250;
-//				
-//				Content.add(FontLabel);
-//				Content.add(SampleLabel);
-//				Content.add(SpacerLabel);
 	        	
 	        					
 			} 
