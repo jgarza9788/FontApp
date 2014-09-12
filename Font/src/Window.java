a0a11c164580b19f5d0794a90e691dca971f9b36
Justin <Justin@justingmacbook2>
2014-09-11 23:32:14 -0700

    Improved Search, and Added Comments

--- Font/src/Window.java
+++ Font/src/Window.java
@@ -11,6 +11,7 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.Toolkit;
 import java.io.File;
@@ -19,6 +20,8 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -34,7 +37,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
-
+//create window
 public class Window
 {
 
@@ -60,16 +63,12 @@ public class Window
 		AllFontLabels.clear();
 		
 		JFrame PleaseWait = new JFrame();
-//		PleaseWait.getContentPane.setlayout(new BorderLayout());
 		PleaseWait.getContentPane().add(new JLabel("Please Wait...Loading Fonts",JLabel.CENTER));
 		
         ThisWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ThisWindow.setVisible(false);
         
-        
-        
         Content.setLayout(new GridBagLayout());
-//        Content.setLayout(new BorderLayout(5,5));
         Content.setBackground(Color.WHITE);
         ScrollPane.getVerticalScrollBar().setUnitIncrement(10);
         
@@ -103,13 +102,13 @@ public class Window
 		
 	}
 	
+	//create SearchBar
 	public void AddSearchBar()
 	{
 		JLabel ThisLabel = new JLabel("Search");
 		SearchArea.add(ThisLabel, BorderLayout.LINE_START);
 		
 		final JTextField SearchBox =  new JTextField(20);
-//		SearchBox.setSize(100,125);
 		SearchArea.add(SearchBox, BorderLayout.LINE_END);
 		
 		SearchBox.addActionListener(new ActionListener() 
@@ -120,25 +119,23 @@ public class Window
 
 		    	  for (int i = 0; i < AllFontLabels.size() ; i ++)
 		    	  {
-		    		  System.out.println(AllFontLabels.get(i).getText().toUpperCase().indexOf(SearchBox.getText().toUpperCase()));
 		    				  
 		    		  if (AllFontLabels.get(i).getText().toUpperCase().indexOf(SearchBox.getText().toUpperCase()) >= 0 )
 		    		  {
 		    			  if (FoundOne == false)
 		    			  {
 			    			  Content.scrollRectToVisible(AllFontLabels.get(AllFontLabels.size() -1).getBounds());
-			    			  Content.scrollRectToVisible(AllFontLabels.get(i).getBounds());
+			    			  Content.scrollRectToVisible(AllFontLabels.get(i).getParent().getBounds());
 			    			  FoundOne = true;
 		    			  }
 
-		    			  AllFontLabels.get(i).setOpaque(true);
-		    			  AllFontLabels.get(i).setBackground(Color.yellow);
+		    			  AllFontLabels.get(i).getParent().setVisible(true);
 
 		    		  }
 		    		  else
 		    		  {
-		    			  AllFontLabels.get(i).setOpaque(false);
-		    			  AllFontLabels.get(i).setBackground(Color.white);
+		    			  AllFontLabels.get(i).getParent().setVisible(false);
+		    			  
 		    		  }
 		    			  
 		    		  
@@ -147,14 +144,16 @@ public class Window
 		});
 	}
 	
+	//get all fonts on computer system
 	public void CollectContent()
 	{
 		
-		// Directory path here
+		// get system fonts
 		String ComputerFontPath = "/Volumes/Macintosh HD/Library/Fonts"; 
 		File ComputerFontfolder = new File(ComputerFontPath);
 		File[] ComputerlistOfFiles = ComputerFontfolder.listFiles(); 
 		
+		//get user fonts
 		String MyFontPath = System.getProperty("user.home") + "/Library/Fonts"; 
 		File MyFontfolder = new File(MyFontPath);
 		File[] MylistOfFiles = MyFontfolder.listFiles(); 
@@ -177,15 +176,26 @@ public class Window
 			}
 		}
 		
+		//get all fonts in 2 lists
 		Collections.sort(AllFontNames.subList(1, AllFontNames.size()));
 		Collections.sort(AllFontPaths.subList(1, AllFontPaths.size()));
 		
 	}
 
+	//add fonts to scrollpane
 	public void AddContent()
 	{
 		int y = 1;
 		String FontPath = null;
+		
+		GridBagConstraints GBC = new GridBagConstraints();
+		GBC.fill = GridBagConstraints.HORIZONTAL;
+		GBC.weightx = 0;
+    	GBC.weighty = 0;
+    	GBC.gridx = 0;
+    	GBC.gridy = y;
+    	GBC.gridwidth = 10;
+    	GBC.insets = new Insets(0,0,0,0);
 
 		for (String F : AllFontNames)
 		{
@@ -201,7 +211,6 @@ public class Window
 				}
 			}
 			
-			System.out.println("Path For Button!: " + FontPath);
 			
 			try 
 			{
@@ -228,51 +237,19 @@ public class Window
 					{
 						FontSize = FontSize + 1;
 					}
-					System.out.println(FontSize);
 					
-					
-				} while (LoopAgain);			
+				} while (LoopAgain);	
 				
-
-				JLabel FontLabel = new JLabel(F);
-				FontLabel.setFont(new Font(font.getName(), Font.PLAIN, FontSize));
-				JLabel SampleLabel = new JLabel("The quick brown fox jumps over the lazy dog");
-				SampleLabel.setFont(new Font(font.getName(), Font.PLAIN, FontSize));
-				JLabel SpacerLabel = new JLabel(" ");
-				SpacerLabel.setFont(new Font(font.getName(), Font.PLAIN, FontSize));
-				
-				AllFontLabels.add(FontLabel);
-					
-				GridBagConstraints GBC = new GridBagConstraints();
-				GBC.fill = GridBagConstraints.HORIZONTAL;
-				GBC.weightx = 0.5;
-	        	GBC.weighty = 0.5;
-	        	GBC.gridx = 0;
-	        	GBC.gridy = y;
-	        	GBC.gridwidth = 10;
-	        	GBC.insets = new Insets(5,10,0,10);
-	        	
-	        	Content.add(FontLabel,GBC);
-	        	
-	        	y = y + 1;
-	        	GBC.gridx = 0;
-	        	GBC.gridy = y;
-	        	GBC.insets = new Insets(0,10,5,10);
-	        	
-	        	Content.add(SampleLabel,GBC);
-	        	
-	        	GBC.fill = GridBagConstraints.NONE;
-				GBC.weightx = 0;
-	        	GBC.weighty = 0.5;
-	        	y = y + 1;
-	        	GBC.gridy = y;
-	        	GBC.anchor = GridBagConstraints.NORTHWEST;
-	        	GBC.insets = new Insets(0,0,0,0);
-	        	
 	        	final String FinalFontPath = FontPath;
-	        	
-	        	JButton OIF = new JButton("Open In Finder");
-	        	Content.add(OIF,GBC);
+				
+				JLabel FontLabel = new JLabel(" " + F); 
+				FontLabel.setFont(new Font(font.getName(), Font.PLAIN, FontSize));
+				
+				JLabel SampleLabel = new JLabel(" Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj...");
+				SampleLabel.setFont(new Font(font.getName(), Font.PLAIN, FontSize));
+
+				
+				JButton OIF = new JButton("Open In Finder");
 	        	OIF.addActionListener(
 	        			new ActionListener( ) 
 	        			{
@@ -284,24 +261,30 @@ public class Window
 							}
 	        			}
 	        			);
+				
+				AllFontLabels.add(FontLabel);
+									
+	        	JPanel FontContainer = new JPanel();
+
+	        	FontContainer.setBackground(Color.white);
 	        	
+	        	FontContainer.setLayout(new BoxLayout(FontContainer,BoxLayout.Y_AXIS));
+	        	
+	        	FontContainer.add(new JLabel(" "));
+	        	FontContainer.add(FontLabel);
+	        	FontContainer.add(SampleLabel);
+	        	FontContainer.add(OIF);
+	        	FontContainer.add(new JLabel(" "));
+	        	
+	        	FontContainer.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.LIGHT_GRAY));
+	        	
+	        	
+	        	Content.add(FontContainer , GBC);
+
+
 	        	y = y + 1;
 	        	GBC.gridy = y;
-	        	GBC.insets = new Insets(0,0,0,0);
-	        	
-	    		JLabel Separator = new JLabel("---------------------------------------------");
-	    		Separator.setForeground(Color.lightGray);
-	    		Content.add(Separator , GBC);
-	        	
-//	        	JSeparator JS = new JSeparator();
-//	        	Content.add(JS,GBC);
-//	        	Content.add(new JSeparator(),GBC);
-//	        	Content.add(new JSeparator(SwingConstants.HORIZONTAL),GBC);
-	        	
-	        	y = y + 1;
-	        	GBC.gridy = y;
-	        	
-	        					
+				
 			} 
 			catch (FontFormatException e) 
 			{
